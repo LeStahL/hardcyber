aa8eb958f5725704208919d1c7600b2185cc56bb
Alexander Kraus <nr4@z10.info>
2019-10-01 00:45:20 +0200

    implemented nonworking stuff.

--- sketch/fconv.c
+++ sketch/fconv.c
@@ -37,7 +37,7 @@
 #include <stdlib.h>
 
 double t_now = 0.,
-    t_start = 0.;
+       t_start = 0.;
 
 PFNGLCREATESHADERPROC glCreateShader;
 PFNGLCREATEPROGRAMPROC glCreateProgram;
@@ -51,46 +51,52 @@ PFNGLUNIFORM1FPROC glUniform1f;
 PFNGLUNIFORM1IPROC glUniform1i;
 PFNGLGENFRAMEBUFFERSPROC glGenFramebuffers;
 PFNGLBINDFRAMEBUFFERPROC glBindFramebuffer;
-    
+PFNGLGETSHADERIVPROC glGetShaderiv;
+PFNGLGETSHADERINFOLOGPROC glGetShaderInfoLog;
+PFNGLGETPROGRAMIVPROC glGetProgramiv;
+PFNGLGETPROGRAMINFOLOGPROC glGetProgramInfoLog;
+PFNGLACTIVETEXTUREPROC glActiveTexture;
+PFNGLFRAMEBUFFERTEXTURE2DPROC glFramebufferTexture2D;
+
 // TODO: remove below
 void debug(int shader_handle)
 {
-	printf("    Debugging shader with handle %d.\n", shader_handle);
-	int compile_status = 0;
-	glGetShaderiv(shader_handle, GL_COMPILE_STATUS, &compile_status);
-	if(compile_status != GL_TRUE)
-	{
-		printf("    FAILED.\n");
-		GLint len;
-		glGetShaderiv(shader_handle, GL_INFO_LOG_LENGTH, &len);
-		printf("    Log length: %d\n", len);
-		GLchar *CompileLog = (GLchar*)malloc(len*sizeof(GLchar));
-		glGetShaderInfoLog(shader_handle, len, NULL, CompileLog);
-		printf("    Error messages:\n%s\n", CompileLog);
-		free(CompileLog);
-	}
-	else
-		printf("    Shader compilation successful.\n");
+    printf("    Debugging shader with handle %d.\n", shader_handle);
+    int compile_status = 0;
+    glGetShaderiv(shader_handle, GL_COMPILE_STATUS, &compile_status);
+    if(compile_status != GL_TRUE)
+    {
+        printf("    FAILED.\n");
+        GLint len;
+        glGetShaderiv(shader_handle, GL_INFO_LOG_LENGTH, &len);
+        printf("    Log length: %d\n", len);
+        GLchar *CompileLog = (GLchar*)malloc(len*sizeof(GLchar));
+        glGetShaderInfoLog(shader_handle, len, NULL, CompileLog);
+        printf("    Error messages:\n%s\n", CompileLog);
+        free(CompileLog);
+    }
+    else
+        printf("    Shader compilation successful.\n");
 }
 
 void debugp(int program)
 {
-	printf("    Debugging program with handle %d.\n", program);
-	int compile_status = 0;
-	glGetProgramiv(program, GL_LINK_STATUS, &compile_status);
-	if(compile_status != GL_TRUE)
-	{
-		printf("    FAILED.\n");
-		GLint len;
-		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &len);
-		printf("    Log length: %d\n", len);
-		GLchar *CompileLog = (GLchar*)malloc(len*sizeof(GLchar));
-		glGetProgramInfoLog(program, len, NULL, CompileLog);
-		printf("    Error messages:\n%s\n", CompileLog);
-		free(CompileLog);
-	}
-	else
-		printf("    Program linking successful.\n");
+    printf("    Debugging program with handle %d.\n", program);
+    int compile_status = 0;
+    glGetProgramiv(program, GL_LINK_STATUS, &compile_status);
+    if(compile_status != GL_TRUE)
+    {
+        printf("    FAILED.\n");
+        GLint len;
+        glGetProgramiv(program, GL_INFO_LOG_LENGTH, &len);
+        printf("    Log length: %d\n", len);
+        GLchar *CompileLog = (GLchar*)malloc(len*sizeof(GLchar));
+        glGetProgramInfoLog(program, len, NULL, CompileLog);
+        printf("    Error messages:\n%s\n", CompileLog);
+        free(CompileLog);
+    }
+    else
+        printf("    Program linking successful.\n");
 }
 #else // DEBUG
 #define printf(a)
@@ -111,44 +117,44 @@ void quad()
     glEnd();
     glFlush();
 }
-    
+
 int flip_buffers()
 {
-	SwapBuffers(hdc);
+    SwapBuffers(hdc);
 
-	MSG msg = { 0 };
-	while ( PeekMessageA( &msg, NULL, 0, 0, PM_REMOVE ) )
-	{
-		if ( msg.message == WM_QUIT ) {
-			return FALSE;
-		}
-		TranslateMessage( &msg );
-		DispatchMessageA( &msg );
-	}
+    MSG msg = { 0 };
+    while ( PeekMessageA( &msg, NULL, 0, 0, PM_REMOVE ) )
+    {
+        if ( msg.message == WM_QUIT ) {
+            return FALSE;
+        }
+        TranslateMessage( &msg );
+        DispatchMessageA( &msg );
+    }
 
-	return TRUE;
+    return TRUE;
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	switch(uMsg)
-	{
-		case WM_KEYDOWN:
-			switch(wParam)
-			{
-				case VK_ESCAPE:
-					ExitProcess(0);
-					break;
-			}
-			break;
-		case WM_RBUTTONDOWN:
-			ExitProcess(0);
-			break;
+    switch(uMsg)
+    {
+    case WM_KEYDOWN:
+        switch(wParam)
+        {
+        case VK_ESCAPE:
+            ExitProcess(0);
+            break;
+        }
+        break;
+    case WM_RBUTTONDOWN:
+        ExitProcess(0);
+        break;
 
-		default:
-			break;
-	}
-	return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    default:
+        break;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
 // Pure opengl drawing code, essentially cross-platform
@@ -156,7 +162,7 @@ void draw()
 {
 //     glActiveTexture(GL_TEXTURE1);
 //     glBindTexture(GL_TEXTURE_2D, font_texture_handle);
-    
+
     quad();
 //     glBindTexture(GL_TEXTURE_2D, 0);
 }
@@ -164,109 +170,191 @@ void draw()
 int WINAPI demo(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
 #ifdef DEBUG
-	AllocConsole();
-	freopen("CONIN$", "r", stdin);
-	freopen("CONOUT$", "w", stdout);
-	freopen("CONOUT$", "w", stderr);
+    AllocConsole();
+    freopen("CONIN$", "r", stdin);
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONOUT$", "w", stderr);
 #endif
-    
+
     // Display demo window
-	CHAR WindowClass[]  = "Team210 Demo Window";
+    CHAR WindowClass[]  = "Team210 Demo Window";
 
-	WNDCLASSEX wc = { 0 };
-	wc.cbSize = sizeof(wc);
-	wc.style = CS_OWNDC | CS_VREDRAW | CS_HREDRAW;
-	wc.lpfnWndProc = &WindowProc;
-	wc.cbClsExtra = 0;
-	wc.cbWndExtra = 0;
-	wc.hInstance = hInstance;
-	wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
-	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = NULL;
-	wc.lpszMenuName = NULL;
-	wc.lpszClassName = WindowClass;
-	wc.hIconSm = NULL;
+    WNDCLASSEX wc = { 0 };
+    wc.cbSize = sizeof(wc);
+    wc.style = CS_OWNDC | CS_VREDRAW | CS_HREDRAW;
+    wc.lpfnWndProc = &WindowProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = NULL;
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = WindowClass;
+    wc.hIconSm = NULL;
 
-	RegisterClassEx(&wc);
+    RegisterClassEx(&wc);
 
-	// Create the window.
+    // Create the window.
     HWND hwnd = CreateWindowEx(
-            0,                                                          // Optional window styles.
-            WindowClass,                                                // Window class
-            ":: Team210 :: GO - MAKE A DEMO ::",                                 // Window text
-            WS_POPUP | WS_VISIBLE,                                      // Window style
-            0,
-            0,
-            w,
-            h,                     // Size and position
+                    0,                                                          // Optional window styles.
+                    WindowClass,                                                // Window class
+                    ":: Team210 :: GO - MAKE A DEMO ::",                                 // Window text
+                    WS_POPUP | WS_VISIBLE,                                      // Window style
+                    0,
+                    0,
+                    w,
+                    h,                     // Size and position
 
-            NULL,                                                       // Parent window
-            NULL,                                                       // Menu
-            hInstance,                                                  // Instance handle
-            0                                                           // Additional application data
-        );
-    
+                    NULL,                                                       // Parent window
+                    NULL,                                                       // Menu
+                    hInstance,                                                  // Instance handle
+                    0                                                           // Additional application data
+                );
+
     DEVMODE dm = { 0 };
     dm.dmSize = sizeof(dm);
     dm.dmPelsWidth = w;
     dm.dmPelsHeight = h;
     dm.dmFields = DM_PELSWIDTH | DM_PELSHEIGHT;
-    
+
     ChangeDisplaySettings(&dm, CDS_FULLSCREEN);
-    
-	// Show it
-	ShowWindow(hwnd, TRUE);
-	UpdateWindow(hwnd);
 
-	// Create OpenGL context
-	PIXELFORMATDESCRIPTOR pfd =
-	{
-		sizeof(PIXELFORMATDESCRIPTOR),
-		1,
-		PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,    //Flags
-		PFD_TYPE_RGBA,        // The kind of framebuffer. RGBA or palette.
-		32,                   // Colordepth of the framebuffer.
-		0, 0, 0, 0, 0, 0,
-		0,
-		0,
-		0,
-		0, 0, 0, 0,
-		24,                   // Number of bits for the depthbuffer
-		8,                    // Number of bits for the stencilbuffer
-		0,                    // Number of Aux buffers in the framebuffer.
-		PFD_MAIN_PLANE,
-		0,
-		0, 0, 0
-	};
+    // Show it
+    ShowWindow(hwnd, TRUE);
+    UpdateWindow(hwnd);
 
-	hdc = GetDC(hwnd);
+    // Create OpenGL context
+    PIXELFORMATDESCRIPTOR pfd =
+    {
+        sizeof(PIXELFORMATDESCRIPTOR),
+        1,
+        PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,    //Flags
+        PFD_TYPE_RGBA,        // The kind of framebuffer. RGBA or palette.
+        32,                   // Colordepth of the framebuffer.
+        0, 0, 0, 0, 0, 0,
+        0,
+        0,
+        0,
+        0, 0, 0, 0,
+        24,                   // Number of bits for the depthbuffer
+        8,                    // Number of bits for the stencilbuffer
+        0,                    // Number of Aux buffers in the framebuffer.
+        PFD_MAIN_PLANE,
+        0,
+        0, 0, 0
+    };
 
-	int  pf = ChoosePixelFormat(hdc, &pfd);
-	SetPixelFormat(hdc, pf, &pfd);
+    hdc = GetDC(hwnd);
 
-	glrc = wglCreateContext(hdc);
-	wglMakeCurrent(hdc, glrc);
+    int  pf = ChoosePixelFormat(hdc, &pfd);
+    SetPixelFormat(hdc, pf, &pfd);
+
+    glrc = wglCreateContext(hdc);
+    wglMakeCurrent(hdc, glrc);
 
     glCreateShader = (PFNGLCREATESHADERPROC) wglGetProcAddress("glCreateShader");
-	glCreateProgram = (PFNGLCREATEPROGRAMPROC) wglGetProcAddress("glCreateProgram");
-	glShaderSource = (PFNGLSHADERSOURCEPROC) wglGetProcAddress("glShaderSource");
-	glCompileShader = (PFNGLCOMPILESHADERPROC) wglGetProcAddress("glCompileShader");
-	glAttachShader = (PFNGLATTACHSHADERPROC) wglGetProcAddress("glAttachShader");
-	glLinkProgram = (PFNGLLINKPROGRAMPROC) wglGetProcAddress("glLinkProgram");
-	glUseProgram = (PFNGLUSEPROGRAMPROC) wglGetProcAddress("glUseProgram");
+    glCreateProgram = (PFNGLCREATEPROGRAMPROC) wglGetProcAddress("glCreateProgram");
+    glShaderSource = (PFNGLSHADERSOURCEPROC) wglGetProcAddress("glShaderSource");
+    glCompileShader = (PFNGLCOMPILESHADERPROC) wglGetProcAddress("glCompileShader");
+    glAttachShader = (PFNGLATTACHSHADERPROC) wglGetProcAddress("glAttachShader");
+    glLinkProgram = (PFNGLLINKPROGRAMPROC) wglGetProcAddress("glLinkProgram");
+    glUseProgram = (PFNGLUSEPROGRAMPROC) wglGetProcAddress("glUseProgram");
     glGetUniformLocation = (PFNGLGETUNIFORMLOCATIONPROC) wglGetProcAddress("glGetUniformLocation");
     glUniform1f = (PFNGLUNIFORM1FPROC) wglGetProcAddress("glUniform1f");
     glUniform1i = (PFNGLUNIFORM1IPROC) wglGetProcAddress("glUniform1i");
     glGenFramebuffers = (PFNGLGENFRAMEBUFFERSPROC) wglGetProcAddress("glGenFramebuffers");
     glBindFramebuffer = (PFNGLBINDFRAMEBUFFERPROC) wglGetProcAddress("glBindFramebuffer");
+    glGetShaderInfoLog = (PFNGLGETSHADERINFOLOGPROC) wglGetProcAddress("glGetShaderInfoLog");
+    glGetShaderiv = (PFNGLGETSHADERIVPROC) wglGetProcAddress("glGetShaderiv");
+    glGetProgramInfoLog = (PFNGLGETPROGRAMINFOLOGPROC) wglGetProcAddress("glGetProgramInfoLog");
+    glGetProgramiv = (PFNGLGETPROGRAMIVPROC) wglGetProcAddress("glGetProgramiv");
+    glActiveTexture = (PFNGLACTIVETEXTUREPROC) wglGetProcAddress("glActiveTexture");
+    glFramebufferTexture2D = (PFNGLFRAMEBUFFERTEXTURE2DPROC) wglGetProcAddress("glFramebufferTexture2D");
 
     ShowCursor(FALSE);
-    
+
+    // Load shader
+    FILE *f = fopen("fconv.frag", "rt");
+    fseek(f, 0, SEEK_END);
+    size_t len = ftell(f);
+    char *source = (char*)malloc(len+1);
+    fseek(f, 0, SEEK_SET);
+    fread(source, 1, len, f);
+    source[len-1] = 0;
+
+    int shader_handle = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(shader_handle, 1, &source, NULL);
+    glCompileShader(shader_handle);
+    debug(shader_handle);
+
+    int program_handle = glCreateProgram();
+    glAttachShader(program_handle, shader_handle);
+    glLinkProgram(program_handle);
+    debugp(program_handle);
+
+    glUseProgram(program_handle);
+    int iDataLocation = glGetUniformLocation(program_handle, "iData"),
+        iDataWidthLocation = glGetUniformLocation(program_handle, "iDataWidth");
+
+    // Setup input
+#define data_width 10
+
+    int idata_texture_handle;
+    unsigned short odata[data_width*data_width],
+             idata[data_width*data_width];
+
+    for(int i=0; i<data_width*data_width; ++i)
+    {
+        idata[i] = i % 2;
+        odata[i] = 0;
+        printf("%hu", idata[i]);
+    }
+    printf("\n");
+
+    glGenTextures(1, &idata_texture_handle);
+    glBindTexture(GL_TEXTURE_2D, idata_texture_handle);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_R16UI, data_width, data_width, 0, GL_RED, GL_UNSIGNED_SHORT, idata);
+
+    // Setup output
+    int output_framebuffer;
+    glGenFramebuffers(1, &output_framebuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, output_framebuffer);
+
+    int odata_texture_handle;
+    glGenTextures(1, &odata_texture_handle);
+    glBindTexture(GL_TEXTURE_2D, odata_texture_handle);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_R16UI, data_width, data_width, 0, GL_RED, GL_UNSIGNED_SHORT, odata);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, odata_texture_handle, 0);
+
     // Main loop
-	t_start = 0.;
-	while(flip_buffers())
-	{
-		draw();
-	}
-	return 0;
+    glViewport(0,0,data_width,data_width);
+
+    glUniform1i(iDataLocation, 0);
+    glUniform1i(iDataWidthLocation, data_width);
+
+    glBindTexture(GL_TEXTURE_2D, idata_texture_handle);
+    glActiveTexture(GL_TEXTURE0);
+
+    draw();
+    
+    glBindTexture(GL_TEXTURE_2D, odata_texture_handle);
+    
+    glReadPixels(0,0,data_width, data_width, GL_RED, GL_UNSIGNED_SHORT, odata);
+    glFlush();
+    
+    for(int i=0; i<data_width*data_width; ++i)
+    {
+        printf("%d: %hu -> %hu\n", i, idata[i], odata[i]);
+    }
+    
+    return 0;
 }
---
+++ sketch/fconv.frag
@@ -0,0 +1,11 @@
+#version 430
+
+uniform usampler2D iData;
+uniform int iDataWidth;
+
+out uint oData;
+
+void main()
+{
+    oData = 1;
+}
